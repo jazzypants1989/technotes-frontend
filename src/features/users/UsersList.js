65bf2b22151f5cc2dd78955aba08edd262c6f200
Jesse Pence <102560332+jazzypants1989@users.noreply.github.com>
2022-09-17 23:28:31 -0700

    front end coming along

--- src/features/users/UsersList.js
+++ src/features/users/UsersList.js
@@ -3,16 +3,16 @@ import User from './User'
 
 const UsersList = () => {
 
-    const { 
-      data: users, 
-      error, 
-      isError, 
-      isLoading, 
-      isSuccess 
+    const {
+        data: users,
+        isLoading,
+        isSuccess,
+        isError,
+        error
     } = useGetUsersQuery(undefined, {
         pollingInterval: 60000,
         refetchOnFocus: true,
-        refetchOnMountOrArgChange: true,
+        refetchOnMountOrArgChange: true
     })
 
     let content
@@ -20,32 +20,32 @@ const UsersList = () => {
     if (isLoading) content = <p>Loading...</p>
 
     if (isError) {
-      content = <p className="errmsg">{error?.data?.message}</p>
+        content = <p className="errmsg">{error?.data?.message}</p>
     }
 
     if (isSuccess) {
 
-      const { ids } = users
+        const { ids } = users
 
-      const tableContent = ids?.length
-        ? ids.map(userId => <User key={userId} userId={userId} />)
-        : null
+        const tableContent = ids?.length
+            ? ids.map(userId => <User key={userId} userId={userId} />)
+            : null
 
-      content = (
-        <table className="table table--users">
-          <thead className="table__thead">
-            <tr>
-              <th scope="col" className="table__th user__username">Username</th>
-              <th scope="col" className="table__th user__roles">Roles</th>
-              <th scope="col" className="table__th user__edit">Edit</th>
-            </tr>
-          </thead>
-          <tbody>
-            {tableContent}
-          </tbody>
-        </table>
-      )
-    } 
+        content = (
+            <table className="table table--users">
+                <thead className="table__thead">
+                    <tr>
+                        <th scope="col" className="table__th user__username">Username</th>
+                        <th scope="col" className="table__th user__roles">Roles</th>
+                        <th scope="col" className="table__th user__edit">Edit</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {tableContent}
+                </tbody>
+            </table>
+        )
+    }
 
     return content
 }
